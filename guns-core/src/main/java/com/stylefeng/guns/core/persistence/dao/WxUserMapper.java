05120e7742b5f63ffa81f263b8548f57abe3ccc8
yushuai <870252122@qq.com>
2019-10-20 00:17:06 +0800

    feat:小程序授权登录+注册

---
+++ guns-core/src/main/java/com/stylefeng/guns/core/persistence/dao/WxUserMapper.java
@@ -0,0 +1,16 @@
+package com.stylefeng.guns.core.persistence.dao;
+
+import com.stylefeng.guns.core.persistence.model.WxUser;
+import com.baomidou.mybatisplus.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 微信用户表 Mapper 接口
+ * </p>
+ *
+ * @author codeGenerator
+ * @since 2019-10-19
+ */
+public interface WxUserMapper extends BaseMapper<WxUser> {
+
+}
---
+++ guns-core/src/main/java/com/stylefeng/guns/core/persistence/service/impl/WxUserServiceImpl.java
@@ -0,0 +1,20 @@
+package com.stylefeng.guns.core.persistence.service.impl;
+
+import com.stylefeng.guns.core.persistence.model.WxUser;
+import com.stylefeng.guns.core.persistence.dao.WxUserMapper;
+import com.stylefeng.guns.core.persistence.service.IWxUserService;
+import com.baomidou.mybatisplus.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 微信用户表 服务实现类
+ * </p>
+ *
+ * @author codeGenerator
+ * @since 2019-10-19
+ */
+@Service
+public class WxUserServiceImpl extends ServiceImpl<WxUserMapper, WxUser> implements IWxUserService {
+
+}
