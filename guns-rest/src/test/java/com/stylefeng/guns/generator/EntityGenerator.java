bf69b07b6877e3d9a70cbf1fbf6f23c1ec7b58d5
yushuai <870252122@qq.com>
2019-10-20 10:53:20 +0800

    feat:小程序授权登录+注册

--- guns-rest/src/test/java/com/stylefeng/guns/generator/EntityGenerator.java
+++ guns-rest/src/test/java/com/stylefeng/guns/generator/EntityGenerator.java
@@ -29,7 +29,7 @@ public class EntityGenerator {
 
         // 全局配置
         GlobalConfig gc = new GlobalConfig();
-        gc.setOutputDir("/Users/yushuai/IdeaProjects/guns/guns-core/src/main/java");//这里写你自己的java目录
+        gc.setOutputDir("/Users/yushuai/IdeaProjects/guns/guns-rest/src/main/java");//这里写你自己的java目录
         gc.setFileOverride(true);//是否覆盖
         gc.setActiveRecord(true);
         gc.setEnableCache(false);// XML 二级缓存
